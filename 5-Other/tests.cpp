165504f96486f024cec8e03f628a173c6b042449
MosZhang7 <927267943@qq.com>
2026-04-14 22:24:50 +0800

    Enhance documentation on unordered_map and class size; refactor tests

--- 5-Other/tests.cpp
+++ 5-Other/tests.cpp
@@ -1,83 +1,22 @@
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <map>
 #include <string>
 #include <string_view>
+#include <sys/socket.h>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 using namespace std;
 
-std::vector<std::string> split(const std::string &s, char delim) {
-  std::vector<std::string> result;
-  std::string current;
-
-  for (char c : s) {
-    if (c == delim) {
-      result.push_back(current);
-      current.clear();
-    } else {
-      current.push_back(c);
-    }
-  }
-  result.push_back(current); // 最后一个字段
-
-  return result;
-}
-
-class Solution {
-public:
-  int compareVersion(string version1, string version2) {
-    string_view v1s(version1);
-    // split('.')形成vector
-    // 每个vector去掉前导0
-    // 将vector给join成一个新的string
-    // 逐位比较，直到到末尾
-    vector<string> v1, v2;
-
-    auto n1 = version1.size();
-    int lp = 0, rp = 0;
-    for (int i = 0; i < n1; i++) {
-      if (version1[i] == '.') {
-        rp = i;
-        auto tmp = version1.substr(lp, rp);
-        while (tmp.starts_with('0')) {
-          tmp = tmp.substr(0, 1);
-        }
-        v1.push_back(tmp);
-        lp = rp;
-      }
-    }
-
-    auto n2 = version2.size();
-    lp = rp = 0;
-    for (int i = 0; i < n2; i++) {
-      if (version2[i] == '.') {
-        rp = i;
-        auto tmp = version2.substr(lp, rp);
-        while (tmp.starts_with('0')) {
-          tmp = tmp.substr(0, 1);
-        }
-        v2.push_back(tmp);
-        lp = rp;
-      }
-    }
-
-    string vv1 = "", vv2 = "";
-    for (auto &v : v1) {
-      vv1 += v;
-    }
-    for (auto &v : v2) {
-      vv2 += v;
-    }
-    auto length = min(vv1.size(), vv2.size());
-    for (int i = 0; i < length; i++) {
-      if (vv1[i] < vv2[i]) {
-        return -1;
-      }
-      if (vv1[i] > vv2[i]) {
-        return 1;
-      }
-    }
-  }
-};
-
 int main() {
-  Solution s;
-  s.compareVersion("1.2", "1.10");
+  unordered_map<pair<int, int>, int> testCases = {
+      {{1, 0}, 0}, {{1, 1}, 0},   {{1, 2}, -1},
+      {{2, 1}, 1}, {{1, 10}, -1}, {{10, 1}, 1},
+  };
+  std::hash<std::string> hash_fn;
+  std::cout << hash_fn("100") << std::endl;
 }
